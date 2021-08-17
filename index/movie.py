01ed8b7c195bd337f17a7e3248b31ca7d8fd9614
Samir millan <samirmillanorozco@hotmil.com>
2021-08-17 21:37:21 +0000

    class movie setter

---
+++ index/movie.py
@@ -0,0 +1,13 @@
+class Movie:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return (f"Movie: {self.name}")
+
+    @property
+    def name(selft):
+        return selft.name
+    @name.setter
+    def name(self, name):
+        self.name = name
